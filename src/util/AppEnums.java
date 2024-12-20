e279cf7100e5f0a48c8fc798faf0b502f6eb17c2
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-20 17:31:38 +0700

    setup enum utils

---
+++ src/util/AppEnums.java
@@ -0,0 +1,20 @@
+enum StatusTransaksi{
+    Pending,
+    Accepted,
+    Rejected,
+    Running,
+    Due,
+    Done
+}
+
+enum RentalInterval{
+    Hour,
+    Day,
+    Week,
+    Month
+}
+
+enum Roles{
+    ADMIN,
+    OPERATOR
+}
