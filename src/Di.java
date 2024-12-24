c07c20af4e5233b60dafd463ae325d0213f93a71
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-24 22:39:31 +0700

    injecttt

--- src/Di.java
+++ src/Di.java
@@ -1,4 +1,33 @@
+import data.schemas.models.Produk;
+import data.schemas.models.Transaksi;
+import data.sources.*;
+import repositories.ProdukRepository;
+import repositories.UserRepository;
+
 public class Di {
-    // TODO @Seta Injection
-    
+    // DATASOURCES
+    ProductDataSource productDataSource;
+    CustomerDataSource customerDataSource;
+    LaporanDataSource laporanDataSource;
+    TransaksiDataSource transaksiDataSource;
+    UserDataSource userDataSource;
+
+    //REPOSITORIES
+    ProdukRepository produkRepository;
+    UserRepository userRepository;
+
+    //PRESENTERS
+
+    //MENUS
+
+    public Di() {
+        productDataSource = new ProductDataSource();
+        customerDataSource = new CustomerDataSource();
+        laporanDataSource = new LaporanDataSource();
+        transaksiDataSource = new TransaksiDataSource();
+        userDataSource = new UserDataSource();
+
+        produkRepository = new ProdukRepository(productDataSource);
+        userRepository = new UserRepository(userDataSource);
+    }
 }
