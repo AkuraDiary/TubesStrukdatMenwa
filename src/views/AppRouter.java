d6f16d2e7f249899f61ec677ffdeb5f84e098e91
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-12-25 09:00:47 +0700

    defining routes

--- src/views/AppRouter.java
+++ src/views/AppRouter.java
@@ -12,9 +12,17 @@ public class AppRouter {
         EXIT,
         // OPERATOR
         OPERATOR_MENU,
+        MASTER_CUSTOMER,
+        TRANSAKSI,
+        PEMBAYARAN,
+        PENGEMBALIAN,
 
         //ADMIN
         ADMiN_MENU,
+        MASTER_OPERATOR,
+        MASTER_LAPORAN,
+        MASTER_APPROVE_TRANSAKSI,
+        MASTER_PRODUK
 
     }
 }
